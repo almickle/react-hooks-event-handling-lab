eb589b06d0b25407dcd9528a32c1303436dbaa23
Michael Dear <michaelcdear@gmail.com>
2022-07-22 13:03:50 -0600

    commit tree 8faaea3d4743779a054b114417a7bc2f732131f8

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+
+    const focus = () => console.log("Good!")
+    const blur = () => console.log("Hey! Eyes on me!")
+    return (
+        <div>
+            <button onFocus={focus} onBlur={blur} >Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
